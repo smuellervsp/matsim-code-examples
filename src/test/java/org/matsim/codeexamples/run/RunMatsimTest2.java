7bf7a45bb539fc38df1a6343b00e031e67b041d4
sebastianmuller <sebastianmuller@sebastians-mbp.fritz.box>
2019-12-02 15:03:08 +0100

    add RunMatsimTest2

---
+++ src/test/java/org/matsim/codeexamples/run/RunMatsimTest2.java
@@ -0,0 +1,43 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * EditRoutesTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2019 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+
+package org.matsim.codeexamples.run;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+* @author smueller
+*/
+
+public class RunMatsimTest2 {
+
+	@Test
+	public void main() {
+		
+		Assert.fail();
+		
+
+	}
+
+}
